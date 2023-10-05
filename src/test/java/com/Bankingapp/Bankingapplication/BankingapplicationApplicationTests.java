b43d2e0db43c7a0fb6e9c74bae4c57655bc37cc5
rogadi <rogadi@LIN33004075.corp.capgemini.com>
2023-10-05 11:16:15 +0530

    First Commit

---
+++ src/test/java/com/Bankingapp/Bankingapplication/BankingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bankingapp.Bankingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
